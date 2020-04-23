44856823dde655cd19ea50899208eba86ad88e7b
Erik Cikalleshi <erik.ck@outlook.it>
2020-04-23 17:59:33 +0200

    Abstract class "Verfahren"

---
+++ src/sample/Verfahren.java
@@ -0,0 +1,28 @@
+package sample;
+
+public abstract class Verfahren {
+    private String function;
+    private int startValue, endValue, accuracy, max;
+
+    abstract double calculation();
+
+    public String getFunction() {
+        return function;
+    }
+
+    public int getStartValue() {
+        return startValue;
+    }
+
+    public int getEndValue() {
+        return endValue;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
+    }
+
+    public int getMax() {
+        return max;
+    }
+}
