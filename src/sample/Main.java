0b45c61fba584db7fdd703f6ad5e73e63f893a2c
suck1n <duckin@protonmail.com>
2020-04-26 14:28:47 +0200

    Add a GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Scenes/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Scenes/MainScene.fxml"));
         primaryStage.setTitle("Näherungsverfahren");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
--- src/sample/Scenes/Controller.java
+++ src/sample/Scenes/Controller.java
@@ -1,5 +1,9 @@
 package sample.Scenes;
 
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import sample.Utility.Point;
 import sample.Verfahren.Bisektion.Bisektionsverfahren;
@@ -11,6 +15,20 @@ public class Controller {
     public TextField txt_EndValue;
     public TextField txt_StartValue;
     public TextField txt_MaxEntries;
+
+    public LineChart<Double, Double> lineChart;
+
+    public ComboBox<String> cb_Verfahren1;
+    public ComboBox<String> cb_Verfahren2;
+    public ComboBox<String> cb_Verfahren3;
+
+    public Label lbl_Verfahren1;
+    public Label lbl_Verfahren2;
+    public Label lbl_Verfahren3;
+
+    public ListView<String> lv_container1;
+    public ListView<String> lv_container2;
+    public ListView<String> lv_container3;
 
     public void btn_Calculate_Click() {
         String function = txt_Function.getText();
