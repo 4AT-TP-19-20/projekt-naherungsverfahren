168ab0c11844a6f358ddc79d1aa22a5bbfa352be
Erik Cikalleshi <stcikeri@bx.fallmerayer.it>
2020-04-26 15:09:04 +0200

    Add RegulaFalsi

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Scenes/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Scenes/MainScene.fxml"));
         primaryStage.setTitle("Näherungsverfahren");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
--- src/sample/Scenes/Controller.java
+++ src/sample/Scenes/Controller.java
@@ -1,13 +1,20 @@
 package sample.Scenes;
 
+import javafx.geometry.Insets;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import sample.Utility.Point;
 import sample.Verfahren.Bisektion.Bisektionsverfahren;
 import sample.Verfahren.Verfahren;
+
+import java.util.ArrayList;
 
 public class Controller {
     public TextField txt_Function;
@@ -32,16 +39,38 @@ public class Controller {
 
     public void btn_Calculate_Click() {
         String function = txt_Function.getText();
-        int accuracy = Integer.parseInt(txt_Accuracy.getText());
-        int endValue = Integer.parseInt(txt_EndValue.getText());
-        int startValue = Integer.parseInt(txt_StartValue.getText());
-        int maxEntries = Integer.parseInt(txt_MaxEntries.getText());
-
-        Verfahren v = new Bisektionsverfahren(function, startValue, endValue, accuracy, maxEntries);
-        Point[] points = v.calculate();
-
-        for(Point p : points) {
-            System.out.println("(" + p.x + "/" + p.y + ")");
+        int accuracy = 0, endValue = 0, startValue = 0, maxEntries = 0;
+        ArrayList<TextField> textFields = new ArrayList<>();
+        textFields.add(txt_Accuracy);
+        textFields.add(txt_StartValue);
+        textFields.add(txt_EndValue);
+        textFields.add(txt_MaxEntries);
+        textFields.add(txt_Function);
+        boolean notEmpty = false;
+        int temp = 0;
+        for (int i = 0; i < 5; i++) {
+            if (textFields.get(i).getText().isEmpty()) {
+                textFields.get(i).setBackground(new Background(new BackgroundFill(Color.INDIANRED, CornerRadii.EMPTY, Insets.EMPTY)));
+            } else {
+                textFields.get(i).setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+                temp++;
+                if(temp == 5){
+                    notEmpty = true;
+                }
+            }
         }
+        if(notEmpty){
+            accuracy = Integer.parseInt(txt_Accuracy.getText());
+            endValue = Integer.parseInt(txt_EndValue.getText());
+            startValue = Integer.parseInt(txt_StartValue.getText());
+            maxEntries = Integer.parseInt(txt_MaxEntries.getText());
+            Verfahren v = new Bisektionsverfahren(function, startValue, endValue, accuracy, maxEntries);
+            Point[] points = v.calculate();
+
+            for (Point p : points) {
+                System.out.println("(" + p.x + "/" + p.y + ")");
+            }
+        }
+
     }
 }
